7a35f10b3957447133d1c45043f443444d8efeee
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-04 19:53:02 -0300

    Use DTO pattern for return Users

--- src/main/java/com/springmongo/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/springmongo/workshopmongo/WorkshopmongoApplication.java
@@ -6,15 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class WorkshopmongoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkshopmongoApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String hello() {
-		return "Hello world nessa caraia, ve se funciona";
 	}
 }
