1a81fd07bc4e583535885603c8138ed837831a7e
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-04 19:26:46 -0300

    Operation instantiate database

---
+++ src/main/java/com/springmongo/workshopmongo/config/Instatiation.java
@@ -0,0 +1,28 @@
+package com.springmongo.workshopmongo.config;
+
+import com.springmongo.workshopmongo.domain.User;
+import com.springmongo.workshopmongo.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class Instatiation implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        repository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        repository.saveAll(Arrays.asList(maria, alex, bob));
+    }
+}
