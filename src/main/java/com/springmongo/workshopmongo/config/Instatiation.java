3580e376f53844760c5e4bce2ec9a8b9e02ab821
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-04 23:01:11 -0300

    data author with DTO

--- src/main/java/com/springmongo/workshopmongo/config/Instatiation.java
+++ src/main/java/com/springmongo/workshopmongo/config/Instatiation.java
@@ -2,6 +2,7 @@ package com.springmongo.workshopmongo.config;
 
 import com.springmongo.workshopmongo.domain.Post;
 import com.springmongo.workshopmongo.domain.User;
+import com.springmongo.workshopmongo.dto.AuthorDTO;
 import com.springmongo.workshopmongo.repositories.PostRepository;
 import com.springmongo.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instatiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", maria);
-
         repository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/springmongo/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,32 @@
+package com.springmongo.workshopmongo.dto;
+
+import com.springmongo.workshopmongo.domain.User;
+
+public class AuthorDTO {
+
+    private String id;
+    private String name;
+
+    public AuthorDTO() {}
+
+    public AuthorDTO(User obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
