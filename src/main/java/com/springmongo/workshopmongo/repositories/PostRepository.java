3f7d09dc2209be00d5181542026e81e14a7cb7e9
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-05 00:13:05 -0300

    Simple query with @Query

--- src/main/java/com/springmongo/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/springmongo/workshopmongo/repositories/PostRepository.java
@@ -2,10 +2,13 @@ package com.springmongo.workshopmongo.repositories;
 
 import com.springmongo.workshopmongo.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    @Query("{'title': { $regex: ?0, $option: 'i'} }")
+    List<Post> searchTitle(String text);
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
