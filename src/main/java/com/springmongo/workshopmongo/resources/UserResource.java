efea70383e51e69f734d6b8fe65d98f72114e83c
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-04 23:12:15 -0300

    Endpoint return posts of users

--- src/main/java/com/springmongo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/springmongo/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.springmongo.workshopmongo.resources;
 
+import com.springmongo.workshopmongo.domain.Post;
 import com.springmongo.workshopmongo.domain.User;
 import com.springmongo.workshopmongo.dto.UserDTO;
 import com.springmongo.workshopmongo.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
