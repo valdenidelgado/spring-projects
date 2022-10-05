4a661fd47244c5bb038ce906ce1046ac5ada8de6
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2022-10-04 21:18:24 -0300

    Delete user with DELETE

--- src/main/java/com/springmongo/workshopmongo/services/UserService.java
+++ src/main/java/com/springmongo/workshopmongo/services/UserService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.SimpleTimeZone;
 
 @Service
 public class UserService {
@@ -27,6 +28,11 @@ public class UserService {
 
     public User insert(User obj) {
         return userRepository.insert(obj);
+    }
+
+    public void delete(String id) {
+        findById(id);
+        userRepository.deleteById(id);
     }
 
     public User fromDTO(UserDTO objDto) {
