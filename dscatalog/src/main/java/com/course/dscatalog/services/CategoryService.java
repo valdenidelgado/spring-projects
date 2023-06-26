ba80f50183972a49364fece8410e9bc33b262cc1
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-06-26 17:10:07 -0300

    database seeding

--- dscatalog/src/main/java/com/course/dscatalog/services/CategoryService.java
+++ dscatalog/src/main/java/com/course/dscatalog/services/CategoryService.java
@@ -4,8 +4,10 @@ import com.course.dscatalog.entities.Category;
 import com.course.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -14,11 +16,16 @@ public class CategoryService {
     private CategoryRepository repository;
 
 
+    @Transactional(readOnly = true)
     public List<Category> findAll() {
         return repository.findAll();
     }
 
     public Category create(Category category) {
         return repository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return repository.findById(id);
     }
 }
