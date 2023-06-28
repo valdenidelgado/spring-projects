03b9077507125eb8a23a2bb76e5f475d67daa83a
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-06-28 19:02:47 -0300

    generate new annotation and custom validation

--- dscatalog/src/main/java/com/course/dscatalog/entities/UserInsertDTO.java
+++ dscatalog/src/main/java/com/course/dscatalog/entities/UserInsertDTO.java
@@ -1,7 +1,9 @@
 package com.course.dscatalog.entities;
 
 import com.course.dscatalog.dto.UserDTO;
+import com.course.dscatalog.services.validation.UserInsertValid;
 
+@UserInsertValid
 public class UserInsertDTO extends UserDTO {
     private String password;
 
---
+++ dscatalog/src/main/java/com/course/dscatalog/services/validation/UserInsertValidator.java
@@ -0,0 +1,48 @@
+package com.course.dscatalog.services.validation;
+
+import com.course.dscatalog.entities.User;
+import com.course.dscatalog.entities.UserInsertDTO;
+import com.course.dscatalog.repositories.UserRepository;
+import com.course.dscatalog.resources.exceptions.FieldMessage;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInsertValidator implements ConstraintValidator<UserInsertValid, UserInsertDTO> {
+
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void initialize(UserInsertValid constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(UserInsertDTO userInsertDTO, ConstraintValidatorContext constraintValidatorContext) {
+        List<FieldMessage> list = new ArrayList<>();
+
+        User user = userRepository.findByEmail(userInsertDTO.getEmail());
+
+        if (user != null) {
+            list.add(new FieldMessage("email", "Email já existe"));
+        }
+
+        if (userInsertDTO.getPassword().length() < 6) {
+            list.add(new FieldMessage("password", "A senha deve ter pelo menos 6 caracteres"));
+        }
+
+
+        for (FieldMessage e : list) {
+            constraintValidatorContext.disableDefaultConstraintViolation();
+            constraintValidatorContext.buildConstraintViolationWithTemplate(e.getMessage())
+                    .addPropertyNode(e.getFieldName())
+                    .addConstraintViolation();
+        }
+        return list.isEmpty();
+    }
+}
