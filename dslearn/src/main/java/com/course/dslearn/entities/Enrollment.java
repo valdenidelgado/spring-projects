32f31a3dc7a071e1e6dccb46d4e79678af3df683
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-06-29 18:35:02 -0300

    fix manytomany lessonsDone

--- dslearn/src/main/java/com/course/dslearn/entities/Enrollment.java
+++ dslearn/src/main/java/com/course/dslearn/entities/Enrollment.java
@@ -2,12 +2,9 @@ package com.course.dslearn.entities;
 
 import com.course.dslearn.entities.pk.EnrollmentPK;
 
-import javax.persistence.Column;
-import javax.persistence.EmbeddedId;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.Instant;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_enrollment")
@@ -23,6 +20,10 @@ public class Enrollment {
     private Instant refundMoment;
     private boolean available;
     private boolean onlyUpdate;
+
+
+    @ManyToMany(mappedBy = "enrollmentsDone")
+    private Set<Lesson> lessonsDone = new HashSet<>();
 
     public Enrollment() {
     }
