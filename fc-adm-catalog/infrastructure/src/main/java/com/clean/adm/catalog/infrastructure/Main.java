200a1f8c67f196b84e3d393fb1a54105521c642b
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-01 18:23:52 -0300

    init

--- fc-adm-catalog/infrastructure/src/main/java/com/clean/adm/catalog/infrastructure/Main.java
+++ fc-adm-catalog/infrastructure/src/main/java/com/clean/adm/catalog/infrastructure/Main.java
@@ -4,6 +4,7 @@ import com.clean.adm.catalog.application.UseCase;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
         System.out.println(new UseCase().execute());
     }
---
+++ test/src/TestRecord.java
@@ -0,0 +1,2 @@
+package src;public record TestRecord() {
+}
