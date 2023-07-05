256831788a4abb5e8ffdc11e479f49a2cce379fb
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-05 15:24:20 -0300

    refactor Category class to extends AggregateRoot and create CategoryID for generate unique ID

--- fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/Category.java
+++ fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/Category.java
@@ -1,10 +1,11 @@
 package com.clean.adm.catalog.domain.category;
 
+import com.clean.adm.catalog.domain.AggregateRoot;
+
 import java.time.Instant;
 import java.util.UUID;
 
-public class Category {
-    private String id;
+public class Category extends AggregateRoot<CategoryID> {
     private String name;
     private String description;
     private boolean active;
@@ -12,32 +13,32 @@ public class Category {
     private Instant updatedAt;
     private Instant deletedAt;
 
-    private Category(final String id,
-                     final String name,
-                     final String description,
-                     final boolean active,
-                     final Instant createdAt,
-                     final Instant updatedAt,
-                     final Instant deletedAt
+    private Category(final CategoryID anId,
+                     final String aName,
+                     final String aDescription,
+                     final boolean isActive,
+                     final Instant aCreatedAt,
+                     final Instant aUpdatedAt,
+                     final Instant aDeletedAt
     ) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.active = active;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.deletedAt = deletedAt;
+        super(anId);
+        this.name = aName;
+        this.description = aDescription;
+        this.active = isActive;
+        this.createdAt = aCreatedAt;
+        this.updatedAt = aUpdatedAt;
+        this.deletedAt = aDeletedAt;
     }
 
     public static Category newCategory(final String aName,
                                        final String aDescription,
-                                       final boolean aIsActive) {
-        final var id = UUID.randomUUID().toString();
+                                       final boolean isActive) {
+        final var id = CategoryID.unique();
         final var now = Instant.now();
-        return new Category(id, aName, aDescription, aIsActive, now, now, null);
+        return new Category(id, aName, aDescription, isActive, now, now, null);
     }
 
-    public String getId() {
+    public CategoryID getId() {
         return id;
     }
 
