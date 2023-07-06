fbdab41dd3d8c30f09eac31fa2f33ebdac0ea037
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-06 15:57:17 -0300

    add tests category

--- fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/Category.java
+++ fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/Category.java
@@ -36,12 +36,42 @@ public class Category extends AggregateRoot<CategoryID> {
                                        final boolean isActive) {
         final var id = CategoryID.unique();
         final var now = Instant.now();
-        return new Category(id, aName, aDescription, isActive, now, now, null);
+        final var deletedAt = isActive ? null : now;
+        return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
+    }
+
+    public Category deactivate() {
+        if (getDeletedAt() == null) {
+            this.deletedAt = Instant.now();
+        }
+
+        this.active = false;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category activate() {
+        this.deletedAt = null;
+        this.active = true;
+        this.updatedAt = Instant.now();
+        return this;
+    }
+
+    public Category update(final String aName, final String aDescription, final boolean isActive) {
+        if (isActive) {
+            activate();
+        } else {
+            deactivate();
+        }
+        this.name = aName;
+        this.description = aDescription;
+        this.updatedAt = Instant.now();
+        return this;
     }
 
     public CategoryID getId() {
@@ -71,4 +101,5 @@ public class Category extends AggregateRoot<CategoryID> {
     public Instant getDeletedAt() {
         return deletedAt;
     }
+
 }
--- fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/CategoryValidator.java
+++ fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/category/CategoryValidator.java
@@ -6,6 +6,8 @@ import com.clean.adm.catalog.domain.validation.Validator;
 
 public class CategoryValidator extends Validator {
 
+    public static final int NAME_MAX_LENGTH = 255;
+    public static final int NAME_MIN_LENGTH = 3;
     private final Category category;
 
     public CategoryValidator(final Category aCategory, final ValidationHandler aHandler) {
@@ -15,8 +17,24 @@ public class CategoryValidator extends Validator {
 
     @Override
     public void validate() {
-        if (category.getName() == null) {
+        checkNameConstraints();
+    }
+
+    private void checkNameConstraints() {
+        final var name = this.category.getName();
+        if (name == null) {
             this.validationHandler().append(new Error("'name' should not be null"));
+            return;
+        }
+
+        if (name.isBlank()) {
+            this.validationHandler().append(new Error("'name' should not be empty"));
+            return;
+        }
+
+        final int length = name.trim().length();
+        if (length > NAME_MAX_LENGTH || length < NAME_MIN_LENGTH) {
+            this.validationHandler().append(new Error("'name' must be between 3 and 255 characters"));
         }
     }
 }
