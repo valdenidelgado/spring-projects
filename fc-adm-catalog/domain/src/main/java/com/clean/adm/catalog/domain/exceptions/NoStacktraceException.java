83d74b26bfae23f4783b47f49d2f66a6039aab97
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-06 14:38:18 -0300

    refactor exceptions

---
+++ fc-adm-catalog/domain/src/main/java/com/clean/adm/catalog/domain/exceptions/NoStacktraceException.java
@@ -0,0 +1,12 @@
+package com.clean.adm.catalog.domain.exceptions;
+
+public class NoStacktraceException extends RuntimeException {
+
+    public NoStacktraceException(final String message) {
+        this(message, null);
+    }
+
+    public NoStacktraceException(final String message, final Throwable cause) {
+        super(message, cause, true, false);
+    }
+}
