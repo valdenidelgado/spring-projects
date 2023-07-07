891f2b945a739a67b924c1dc433bd396cd44dfd0
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-07 13:48:22 -0300

    add configuration to run app

--- fc-adm-catalog/infrastructure/src/main/java/com/clean/adm/catalog/infrastructure/Main.java
+++ fc-adm-catalog/infrastructure/src/main/java/com/clean/adm/catalog/infrastructure/Main.java
@@ -1,10 +1,13 @@
 package com.clean.adm.catalog.infrastructure;
 
-import com.clean.adm.catalog.application.UseCase;
+import com.clean.adm.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        SpringApplication.run(WebServerConfig.class, args);
     }
 }
---
+++ fc-adm-catalog/infrastructure/src/main/java/com/clean/adm/catalog/infrastructure/configuration/WebServerConfig.java
@@ -0,0 +1,9 @@
+package com.clean.adm.catalog.infrastructure.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.clean.adm.catalog")
+public class WebServerConfig {
+}
