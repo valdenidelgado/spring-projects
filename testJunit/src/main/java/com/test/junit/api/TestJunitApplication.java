3e193cadefc5e4e5bc64267b076a92256466d609
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-11 22:31:52 -0300

    init junit project

---
+++ testJunit/src/main/java/com/test/junit/api/TestJunitApplication.java
@@ -0,0 +1,13 @@
+package com.test.junit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestJunitApplication.class, args);
+    }
+
+}
---
+++ testJunit/src/main/java/com/test/junit/api/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.test.junit.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+
+}
