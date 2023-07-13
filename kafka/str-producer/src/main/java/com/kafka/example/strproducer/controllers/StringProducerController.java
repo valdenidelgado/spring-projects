6614e3bfedc7b9bf7b043b1672756b0b4afecc68
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-13 13:37:04 -0300

    finish producer project

---
+++ kafka/str-producer/src/main/java/com/kafka/example/strproducer/controllers/StringProducerController.java
@@ -0,0 +1,24 @@
+package com.kafka.example.strproducer.controllers;
+
+import com.kafka.example.strproducer.services.StringProducerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(value = "/producer")
+public class StringProducerController {
+
+    private final StringProducerService stringProducerService;
+
+    @PostMapping
+    public ResponseEntity<?> sendMessage(@RequestBody String message) {
+        stringProducerService.sendMessage(message);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+}
