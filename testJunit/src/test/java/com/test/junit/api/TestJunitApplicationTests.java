1bd221e848e382e92ba9d06438054995ceccb86f
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-14 15:42:20 -0300

    init test main tdd project

--- testJunit/src/test/java/com/test/junit/api/TestJunitApplicationTests.java
+++ testJunit/src/test/java/com/test/junit/api/TestJunitApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TestJunitApplicationTests {
 
     @Test
-    void contextLoads() {
+    void main() {
+        TestJunitApplication.main(new String[] {});
     }
-
 }
