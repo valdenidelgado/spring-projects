f46727686458f6c7c526ca7d5759733a3d85a66f
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-14 21:16:39 -0300

    start project

---
+++ tdd-clean-solid/src/main/java/com/example/tddcleansolid/TddCleanSolidApplication.java
@@ -0,0 +1,13 @@
+package com.example.tddcleansolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCleanSolidApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddCleanSolidApplication.class, args);
+    }
+
+}
---
+++ tdd-clean-solid/src/test/java/com/example/tddcleansolid/TddCleanSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tddcleansolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddCleanSolidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
