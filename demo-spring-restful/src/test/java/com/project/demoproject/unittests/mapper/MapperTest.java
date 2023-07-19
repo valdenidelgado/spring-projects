e4d5d0d7cf025a2dd500b7b8dcd685335547a42c
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-19 20:02:10 -0300

    add test to MapperStruct Instance

---
+++ demo-spring-restful/src/test/java/com/project/demoproject/unittests/mapper/MapperTest.java
@@ -0,0 +1,97 @@
+package com.project.demoproject.unittests.mapper;
+
+import com.project.demoproject.mapper.MapperStruct;
+import com.project.demoproject.model.Person;
+import com.project.demoproject.model.dto.v1.PersonDTO;
+import com.project.demoproject.unittests.mapper.mocks.MockPerson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MapperTest {
+    MockPerson inputObject;
+
+    @BeforeEach
+    public void setUp() {
+        inputObject = new MockPerson();
+    }
+
+    @Test
+    public void parseEntityToDTOTest() {
+        PersonDTO output = MapperStruct.INSTANCE.toPersonDTO(inputObject.mockEntity());
+        assertEquals(Long.valueOf(0L), output.getKey());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAddress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parseEntityListToDTOListTest() {
+        List<PersonDTO> outputList = MapperStruct.INSTANCE.toPersonDTOs(inputObject.mockEntityList());
+        PersonDTO outputZero = outputList.get(0);
+
+        assertEquals(Long.valueOf(0L), outputZero.getKey());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAddress());
+        assertEquals("Male", outputZero.getGender());
+
+        PersonDTO outputSeven = outputList.get(7);
+
+        assertEquals(Long.valueOf(7L), outputSeven.getKey());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAddress());
+        assertEquals("Female", outputSeven.getGender());
+
+        PersonDTO outputTwelve = outputList.get(12);
+
+        assertEquals(Long.valueOf(12L), outputTwelve.getKey());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAddress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+
+    @Test
+    public void parseVOToEntityTest() {
+        Person output = MapperStruct.INSTANCE.toPerson(inputObject.mockDTO());
+        assertEquals(Long.valueOf(0L), output.getId());
+        assertEquals("First Name Test0", output.getFirstName());
+        assertEquals("Last Name Test0", output.getLastName());
+        assertEquals("Addres Test0", output.getAddress());
+        assertEquals("Male", output.getGender());
+    }
+
+    @Test
+    public void parserDTOListToEntityListTest() {
+        List<Person> outputList = MapperStruct.INSTANCE.toPersons(inputObject.mockDTOList());
+        Person outputZero = outputList.get(0);
+
+        assertEquals(Long.valueOf(0L), outputZero.getId());
+        assertEquals("First Name Test0", outputZero.getFirstName());
+        assertEquals("Last Name Test0", outputZero.getLastName());
+        assertEquals("Addres Test0", outputZero.getAddress());
+        assertEquals("Male", outputZero.getGender());
+
+        Person outputSeven = outputList.get(7);
+
+        assertEquals(Long.valueOf(7L), outputSeven.getId());
+        assertEquals("First Name Test7", outputSeven.getFirstName());
+        assertEquals("Last Name Test7", outputSeven.getLastName());
+        assertEquals("Addres Test7", outputSeven.getAddress());
+        assertEquals("Female", outputSeven.getGender());
+
+        Person outputTwelve = outputList.get(12);
+
+        assertEquals(Long.valueOf(12L), outputTwelve.getId());
+        assertEquals("First Name Test12", outputTwelve.getFirstName());
+        assertEquals("Last Name Test12", outputTwelve.getLastName());
+        assertEquals("Addres Test12", outputTwelve.getAddress());
+        assertEquals("Male", outputTwelve.getGender());
+    }
+}
---
+++ demo-spring-restful/src/test/java/com/project/demoproject/unittests/mockito/services/PersonServiceTest.java
@@ -0,0 +1,32 @@
+package com.project.demoproject.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class PersonServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
