84e523e384d8454805935123b2c3413047a29117
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-21 09:54:23 -0300

    create dtos to account credentials and token

---
+++ demo-spring-restful/src/main/java/com/project/demoproject/model/dto/v1/security/AccountCredentialsDTO.java
@@ -0,0 +1,22 @@
+package com.project.demoproject.model.dto.v1.security;
+
+public class AccountCredentialsDTO {
+
+    private String username;
+    private String password;
+
+    public AccountCredentialsDTO() {}
+
+    public AccountCredentialsDTO(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() { return username; }
+
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+
+    public void setPassword(String password) { this.password = password; }
+}
---
+++ demo-spring-restful/src/main/java/com/project/demoproject/model/dto/v1/security/TokenDTO.java
@@ -0,0 +1,72 @@
+package com.project.demoproject.model.dto.v1.security;
+
+import java.util.Date;
+
+public class TokenDTO {
+
+    private String token;
+    private Boolean authenticated;
+    private  Date created;
+    private Date expiration;
+    private String accessToken;
+    private String refreshToken;
+
+    public TokenDTO() {}
+
+    public TokenDTO(String token, Boolean authenticated, Date created, Date expiration, String accessToken, String refreshToken) {
+        this.token = token;
+        this.authenticated = authenticated;
+        this.created = created;
+        this.expiration = expiration;
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Boolean getAuthenticated() {
+        return authenticated;
+    }
+
+    public void setAuthenticated(Boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+}
