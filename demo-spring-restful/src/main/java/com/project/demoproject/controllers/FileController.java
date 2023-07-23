3b902f92439e7b74a584fdeda35cdcd937ac000e
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-07-23 20:43:07 -0300

    add upload and download files resources to api

---
+++ demo-spring-restful/src/main/java/com/project/demoproject/controllers/FileController.java
@@ -0,0 +1,69 @@
+package com.project.demoproject.controllers;
+
+import com.project.demoproject.model.dto.v1.UploadFileResponseDTO;
+import com.project.demoproject.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+    private Logger logger = LoggerFactory.getLogger(FileController.class);
+
+    @Autowired
+    private FileStorageService service;
+
+    @PostMapping("/uploadFile")
+    public UploadFileResponseDTO uploadFile(@RequestParam("file") MultipartFile file) {
+        logger.info("Storing file to disk");
+        var fileName = service.storeFile(file);
+        var fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+                .path("/api/file/v1/downloadFile/")
+                .path(fileName)
+                .toUriString();
+        return new UploadFileResponseDTO(fileName, fileDownloadUri, file.getContentType(), file.getSize());
+    }
+
+    @PostMapping("/uploadMultipleFiles")
+    public List<UploadFileResponseDTO> uploadMultipleFiles(@RequestParam("files") MultipartFile[] files) {
+        logger.info("Storing files to disk");
+        return Arrays.stream(files).map(this::uploadFile).collect(Collectors.toList());
+    }
+
+    @GetMapping("/downloadFile/{fileName:.+}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName, HttpServletRequest request) {
+        logger.info("Reading a file on disk");
+        Resource resource = service.loadFileAsResource(fileName);
+        String contentType = "";
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (Exception ignored) {
+            logger.error("Could not determine file type.");
+        }
+
+        if (contentType.isBlank()) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+}
