27a8d0efc073917f5040343d19b05ace42fa72a8
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-08-31 17:20:48 -0300

    add newitems page

--- fullstack-app/frontend/reactnotes/src/main.jsx
+++ fullstack-app/frontend/reactnotes/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import {ThemeProvider} from 'styled-components'
 import GlobalStyles from './styles/global'
 import theme from './styles/theme.js'
-import {Profile} from "./pages/Profile/index.jsx";
+import {New} from "./pages/New/index.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile/>
+      <New/>
     </ThemeProvider>
   </React.StrictMode>,
 )
--- fullstack-app/frontend/reactnotes/src/pages/New/index.jsx
+++ fullstack-app/frontend/reactnotes/src/pages/New/index.jsx
@@ -2,6 +2,9 @@ import {Container, Form} from "./styles.js";
 import {Header} from "../../components/Header/index.jsx";
 import {Input} from "../../components/Input/index.jsx";
 import {Textarea} from "../../components/Textarea/index.jsx";
+import {Section} from "../../components/Section/index.jsx";
+import {NoteItem} from "../../components/NoteItem/index.jsx";
+import Button from "../../components/Button/index.jsx";
 
 export function New() {
   return (
@@ -17,6 +20,20 @@ export function New() {
 
           <Input placeholder="Título" />
           <Textarea placeholder="Observações " />
+
+          <Section title="Links úteis">
+            <NoteItem value="https://google.com/" />
+            <NoteItem isNew placeholder="Novo link" />
+          </Section>
+
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="react" />
+              <NoteItem isNew placeholder="Nova tag" />
+            </div>
+          </Section>
+
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
