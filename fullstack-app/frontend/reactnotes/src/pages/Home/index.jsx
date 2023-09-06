530dba2dba4341d94947fa18d09e5b7c2be3b49b
Valdeni Delgado <valdenidelgadofilho@gmail.com>
2023-09-06 09:33:53 -0300

    feat: add selected tags

--- fullstack-app/frontend/reactnotes/src/pages/Home/index.jsx
+++ fullstack-app/frontend/reactnotes/src/pages/Home/index.jsx
@@ -5,8 +5,34 @@ import {ButtonText} from "../../components/ButtonText/index.jsx";
 import {Input} from "../../components/Input/index.jsx";
 import {Section} from "../../components/Section/index.jsx";
 import {Note} from "../../components/Note/index.jsx";
+import {useEffect, useState} from "react";
+import {api} from "../../services/api";
 
 export function Home() {
+  const [tags, setTags] = useState([])
+  const [tagSelected, setTagSelected] = useState([])
+
+  function handleTagSelected(tagName) {
+    const alreadySelected = tagSelected.includes(tagName)
+
+    if (alreadySelected) {
+      const filteredTags = tagSelected.filter(tag => tag !== tagName)
+      setTagSelected(filteredTags)
+    } else {
+      setTagSelected(prevState => [...prevState, tagName])
+    }
+  }
+
+  useEffect(() => {
+    // add groupby no backend para não ter tag duplicada
+    async function fetchTags() {
+      const response = await api.get('/tags');
+      setTags(response.data);
+    }
+
+    fetchTags();
+  }, []);
+
   return (
     <Container>
       <Brand>
@@ -16,12 +42,17 @@ export function Home() {
       <Header/>
 
       <Menu>
-        <li><ButtonText title="Todos" isActive/></li>
-        <li><ButtonText title="React"/></li>
-        <li><ButtonText title="Nodejs"/></li>
+        <li><ButtonText title="Todos" onClick={() => handleTagSelected("all")} isActive={tagSelected.length === 0}/>
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}><ButtonText title={tag.name} onClick={() => handleTagSelected(tag.name)}
+                                                 isActive={tagSelected.includes(tag.name)}/></li>
+          ))
+        }
       </Menu>
       <Search>
-        <Input placeholder="Pesquisar pelo título." icon={FiSearch} />
+        <Input placeholder="Pesquisar pelo título." icon={FiSearch}/>
       </Search>
       <Content>
         <Section title="Minhas Notas">
@@ -31,11 +62,11 @@ export function Home() {
               {id: 1, name: 'React'},
               {id: 2, name: 'Node'},
             ]
-          }} />
+          }}/>
         </Section>
       </Content>
       <NewNote to="/new">
-        <FiPlus />
+        <FiPlus/>
         Criar nota
       </NewNote>
     </Container>
